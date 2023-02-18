05529fe5573aaff315c4f26b24d1a90aa3ac93bc
Ubong <ubongjohnimperial@gmail.com>
2023-02-18 13:16:08 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,8 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+
 {
-	puts ("/"Programming is like building a muitilingual puzzle);
+	puts("/"Programming is like building a muitilingual puzzle);
 	return (0);
 }
