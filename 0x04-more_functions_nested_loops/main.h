f6b2fcc9fd4bfc3c1e4bd0905ce6bc4cdab5f583
Ubong <ubongjohnimperial@gmail.com>
2023-02-23 15:44:55 -0800

    isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,7 +4,7 @@ int _isupper(int c);
 
 int _isdigit(int c);
 
-int mul(int a; int b);
+int mul(int a, int b);
 
 void print_numbers(void);
 
@@ -12,9 +12,9 @@ void print_most_numbers(void);
 
 void more_numbers(void);
 
-void print_lines(int n);
+void print_line(int n);
 
-void print_diagonally(int n);
+void print_diagonal(int n);
 
 void print_square(int size);
 
