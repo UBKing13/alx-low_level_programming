b9ce23981712697ae62b03fb0532ff4811a22fdd
Ubong <ubongjohnimperial@gmail.com>
2023-02-28 03:16:34 -0800

    A good engineer thinks in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_sring - Reverses a string
+ * @s: The string to be modified
+ * Return: void
+ */
+
+void rev_sring(char *s)
+
+{
+
+	int len = 0, index = 0;
+	char tmp;
+
+	while (s[index++])
+	len++;
+
+	for (index = len - 1; index >= len / 2; index--)
+	{
+	tmp = s[index];
+	s[index] = s[len - index -1];
+	s[len - index - 1] = tmp;
+	}
+}
